feb3b1289c468bc67d359a9132a08cd6f714c7ab
francomirko91 <francomirko91@gmail.com>
2023-04-24 11:13:26 +0100

    added flow

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.retrofit
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.retrofit.databinding.ActivityMainBinding
 import com.squareup.picasso.Picasso
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,17 +25,22 @@ class MainActivity : AppCompatActivity() {
 
 
         viewModel.retrieveNetworkCall()
-        viewModel.dogResult.observe(this){dogResult ->
-            when (dogResult){
-                is ApiResponse.Loading -> Toast.makeText(this,"LOADING",Toast.LENGTH_SHORT).show()
-                is ApiResponse.Error -> Toast.makeText(this,"ERROR",Toast.LENGTH_SHORT).show()
-                is ApiResponse.Success<MyDataItem> -> Picasso.get().load(dogResult.body?.message).into(binding.imageView)
+
+        lifecycleScope.launch {
+            viewModel.dogResult.collect { dogResult ->
+                when (dogResult) {
+                    is ApiResponse.Loading -> Toast.makeText(applicationContext, "LOADING", Toast.LENGTH_SHORT)
+                        .show()
+                    is ApiResponse.Error -> Toast.makeText(applicationContext, "ERROR", Toast.LENGTH_SHORT).show()
+                    is ApiResponse.Success<MyDataItem> -> Picasso.get()
+                        .load(dogResult.body?.message).into(binding.imageView)
+                }
+            binding.imageView.visibility = View.VISIBLE
             }
+
         }
-        binding.imageView.visibility = View.VISIBLE
 
     }
-
 }
 
 
--- app/src/main/java/com/example/retrofit/MainViewModel.kt
+++ app/src/main/java/com/example/retrofit/MainViewModel.kt
@@ -9,6 +9,8 @@ import androidx.lifecycle.viewModelScope
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -16,9 +18,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainViewModel: ViewModel() {
 
 
-    private var _dogResult = MutableLiveData<ApiResponse<MyDataItem>>()
-    val dogResult : LiveData<ApiResponse<MyDataItem>>
+    private var _dogResult = MutableStateFlow<ApiResponse<MyDataItem>>(ApiResponse.Loading)
+    val dogResult : StateFlow<ApiResponse<MyDataItem>>
             get() = _dogResult
+
+
 
 
     private val retrofit = Retrofit.Builder().baseUrl("https://dog.ceo/api/")
@@ -29,22 +33,22 @@ class MainViewModel: ViewModel() {
 
 
     fun retrieveNetworkCall (){
-        _dogResult.postValue(ApiResponse.Loading)
+        _dogResult.value = ApiResponse.Loading
         viewModelScope.launch {
             try {
                 val response = dogService.getData()
                 if (response.isSuccessful) {
                     val dogImage = response.body()
-                    _dogResult.postValue(ApiResponse.Success(response.code(),dogImage))
+                    _dogResult.value = ApiResponse.Success(response.code(),dogImage)
                     Log.e("MainActivity", "Response successful ${response.code()}")
 
                 } else {
-                    _dogResult.postValue(ApiResponse.Error(response.code(),response.message()))
+                    _dogResult.value = ApiResponse.Error(response.code(),response.message())
                     Log.e("MainActivity", "Response not successful ${response.code()}")
                 }
 
             } catch (e: Exception) {
-                _dogResult.postValue(ApiResponse.Error(500,"Problem"))
+                _dogResult.value = ApiResponse.Error(500,"Problem")
                 Log.e("MainActivity", "Error: ${e.message}")
             }
 
